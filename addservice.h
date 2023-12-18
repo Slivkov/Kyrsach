1a32e51400a30b25686837e5575cc0140f9471b7
Илюша <137887413+Slivkov@users.noreply.github.com>
2023-12-18 08:14:01 +0300

    addservice.h

--- addservice.h
+++ addservice.h
@@ -1,5 +1,5 @@
-#ifndef ADDSERVICE_H
-#define ADDSERVICE_H
+#ifndef ADDSERVICE_H // проверка наличия
+#define ADDSERVICE_H // без него сломается
 
 #include <QDialog>
 #include <QLineEdit>
@@ -7,23 +7,23 @@
 
 class AddService : public QDialog
 {
-    Q_OBJECT
+    Q_OBJECT // без него не работает. Для работы со слотами
 
 public:
     AddService(QWidget *parent = nullptr);
 
-    QString getServiceName() const;
-    QString getSpecialistName() const;
-    QString getServiceCost() const;
-    QString getServiceTime() const;
+    QString getServiceName() const; // Функц. для получения названия услуги
+    QString getSpecialistName() const; // ДЛя имени спеца
+    QString getServiceCost() const; // Для стоимости
+    QString getServiceTime() const; // Для времени
 
 private:
-    QLineEdit *serviceNameLineEdit;
+    QLineEdit *serviceNameLineEdit; // 4 строки - поля ввода
     QLineEdit *specialistNameLineEdit;
     QLineEdit *serviceCostLineEdit;
     QLineEdit *serviceTimeLineEdit;
-    QPushButton *addButton;
+    QPushButton *addButton; // добавить и отмена
     QPushButton *cancelButton;
 };
 
-#endif // ADDSERVICE_H
+#endif // ADDSERVICE_H - конец определения класса
